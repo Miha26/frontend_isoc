fe3b85df4bcb0cb175a5a6e410fea6abaffabd6a
Miha26 <mihaela.potopea03@e-uvt.ro>
2025-06-10 01:45:54 +0300

    fix: adăugat https în URLr

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ async function login() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    const res = await fetch("gatewayisoc-production.up.railway.app/api/login", {
+    const res = await fetch("https://gatewayisoc-production.up.railway.app/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
