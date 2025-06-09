9b28b1fcff6ce00171cdef02c8b7f06240b4c394
Miha26 <mihaela.potopea03@e-uvt.ro>
2025-06-10 00:07:31 +0300

    First commit cu frontend actualizat

---
+++ register.js
@@ -0,0 +1,17 @@
+async function register() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const res = await fetch("gatewayisoc-production.up.railway.app/api/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+    });
+
+    if (res.ok) {
+        alert("Înregistrare reușită! Mergi la login.");
+        window.location.href = "login.html";
+    } else {
+        alert("Eroare la înregistrare");
+    }
+}
